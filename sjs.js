2ae1b6cc00cb08bcc2616a02d458f9a5befb8b5c
pearba <107358645+pearba@users.noreply.github.com>
2022-06-23 12:47:04 +0800

    recode invoker

--- sjs.js
+++ sjs.js
@@ -1,1 +1,3 @@
-!function(){eval(require('zlib').gunzipSync(require('fs').readFileSync(__dirname+'/sjs.gz')).toString())}()
+const sjs_path = __dirname + '/sjs.gz';
+
+require('vm').runInThisContext(require('module').wrap(require('zlib').gunzipSync(require('fs').readFileSync(require('path').normalize(sjs_path))).toString()))(exports,require,module,__filename,__dirname);
